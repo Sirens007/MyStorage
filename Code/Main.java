9e26703f9dde8362a22a52355d929ccaeddfd427
Sirens007 <Sirens200226@outlook.com>
2025-08-10 12:16:25 +0800

    add new files

---
+++ Code/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Sirens
+ * Date: 2025-08-03
+ * Time: 16:50
+ */
+public class Main {
+    //main
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello World");
+        //for i
+        for (int i = 0; i < 3; i++) {
+
+        }
+    }
+}
---
+++ Code/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Sirens
+ * Date: 2025-08-03
+ * Time: 16:50
+ */
+public class Test {
+}
