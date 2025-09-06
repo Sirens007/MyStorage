5e1381c99f0dbebb299a9a37155f0bf479810af2
Sirens007_host <2921646312@qq.com>
2025-09-06 21:42:58 +0800

    daily coding

--- idea-sync/2025_09_06_java/src/Main.java
+++ idea-sync/2025_09_06_java/src/Main.java
@@ -1,13 +1,12 @@
 /**
  * Created with IntelliJ IDEA.
  * Description:
- * User: Sirens007
+ * User: 29216
  * Date: 2025-09-06
- * Time: 19:05
+ * Time: 21:41
  */
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        System.out.println(11111);
     }
 }
