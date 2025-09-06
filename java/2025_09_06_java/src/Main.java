93efff5a37517a6346e93723a05c0c80a7e0b774
Sirens007 <2921646312@qq.com>
2025-09-06 23:02:36 +0800

    test

--- java/2025_09_06_java/src/Main.java
+++ java/2025_09_06_java/src/Main.java
@@ -2,8 +2,8 @@
  * Created with IntelliJ IDEA
  * Description:
  * User: Sirens007
- * Date: 2025/9/6 
- * Time: 22:36
+ * Date: 2025/9/6
+ * Time: 23:01
  */
 public class Main {
     public static void main(String[] args) {
