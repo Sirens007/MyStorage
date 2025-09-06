cd5d4e046f97dcf6bcf718c9c672f8eae3864998
Sirens007 <2921646312@qq.com>
2025-09-06 22:42:38 +0800

    daily coding

---
+++ java/2025_09_06_java/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA
+ * Description:
+ * User: Sirens007
+ * Date: 2025/9/6 
+ * Time: 22:36
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
